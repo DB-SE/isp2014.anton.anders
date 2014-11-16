3cd250c695b8c9fd3c2931d51cca1aaab559ab77
Anton Anders <anton-reg@live.com>
2014-11-16 19:10:10 +0100

    Aufgabe1: Model-Ausgabe wurde generiert

--- Aufgabe1/model.m
+++ Aufgabe1/model.m
@@ -1,2 +1,26 @@
-beDone : [Base] :: _beDone ;
+beDone : Base [Eingabe] Bearbeiten* Ansehen+ Speicher :: _beDone ;
+
+Base : [Tag] Text :: _Base ;
+
+Bearbeiten : Löschen
+	| erledigt ;
+
+Ansehen : live+ :: live_
+	| export+ :: export_ ;
+
+live : GUI
+	| WEB
+	| Konsole ;
+
+export : HTML
+	| Markdown
+	| txt ;
+
+txt : [csv] :: _txt ;
+
+Speicher : SQLDB
+	| TXT ;
+
+SQLDB : sqlite
+	| externMySQL ;
 
