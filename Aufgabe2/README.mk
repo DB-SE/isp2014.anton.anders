6ba8cfeb5c67700161a74c69ac1186b567ff74ed
Anton Anders <anton-reg@live.com>
2014-11-16 19:21:54 +0100

    Konfigurationen beschrieben

---
+++ Aufgabe2/README.mk
@@ -0,0 +1,14 @@
+# beDone-Konfigurationen für Laufzeitvariabilität:
+
+## Feature 1: 
+Eingabe der TODOs über die Konsole
+
+java beDoneLaufzeit.App -finput=console
+
+## Feature 2: 
+Ausgabe der TODOs über die Konsole in einer Tabelle
+java beDoneLaufzeit.App -foutput=console
+
+## Beide Features nochmal zusammen:
+
+java beDoneLaufzeit.App -foutput=console -finput=console
